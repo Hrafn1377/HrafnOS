4d70176c10c2821f50f3d325b93313c2234be935
Hrafn <harmon.james1377@outlook.com>
2026-06-05 14:46:41 -0400

    Clean boot, serial output stable, hardcoded memory assumption

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,10 +1,8 @@
 #include "serial.hpp"
 
 extern "C" void kmain() {
-    kprint("HrafnOS is alive!\n");
-    kprint_uint(42);
-    kprint_char('\n');
-    kprint_hex(0xDEADBEEF);
-    kprint_char('\n');
+    kprint("HrafnOS booting...\n");
+    kprint("Memory: assuming 128MB available at 0x0\n");
+    kprint("Kernel loaded OK.\n");
     while (true) asm volatile("hlt");
 }
